59d1c6b90e89bc82c242b8db553f748f5a3bd6f4
Charlie Fadness <boy_916@hotmail.com>
2021-08-10 11:41:49 -0700

    updated try catch

--- modules/weather.js
+++ modules/weather.js
@@ -17,10 +17,11 @@ async function getWeather(request, response) {
     console.log('inside weather backend');
     // let getLat = 47
     // let getLon = -122
-
+    console.log('lon', getlon, 'lat', getLat);
     multiDayWeather = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${getLat}&lon=${getLon}&key=${process.env.WEATHER_API_KEY}`);
 
     console.log(multiDayWeather.data.data);
+
     response.send(multiDayWeather.data.data.map(day => new Forecast(day)));
   } catch (error) {
     console.log(`Error Function/response: ${error}`)
